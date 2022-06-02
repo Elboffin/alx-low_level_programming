b791c751a5ec42e777783c66fc5df9e1c3b785ca
elboffin <olamide.adetula.1744@gmail.com>
2022-06-02 16:16:35 -0700

    Put

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ *
+ *  main - Entry point
+ *
+ *  
+ *
+ *  Description: puts "Programming is like building a multilingual puzzle"
+ *
+ * 
+ *
+ *  Return: Always return 0 (Success)
+ *
+ */
+
+int main(void)
+
+{
+
+		puts("\"Programming is like building a multilingual puzzle");
+
+			return (0);
+
+}
