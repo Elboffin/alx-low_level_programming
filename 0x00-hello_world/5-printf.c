b6d4b16f6bb69aec848abf692d2550cfaad31789
elboffin <olamide.adetula.1744@gmail.com>
2022-06-02 16:38:52 -0700

    printf command"

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,30 +1,18 @@
 #include <stdio.h>
 
-
-
-/**
+/*
+ * main - entry point
  *
- *  * main - Entry point
+ * Description : printf
  *
- *   *
+ * Return 0 (success)
  *
- *    * Description: prints "with proper grammar, but the outcome is a piece of
- *
- *     * art,"
- *
- *      *
- *
- *       * Return: Always 0 (Success)
- *
- *        */
-
-int main(void)
-
-{
-
-		printf("with proper grammar, but the outcome is a piece of art,\n");
-
-			return (0);
+ * */
+ 
+ int main (void)
+{ 
+	printf("with proper grammar, but the outcome is apiece of art,\n");
+	return(0);
 
 }
 
