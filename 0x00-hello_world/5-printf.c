36b96ad03b546559bf2b527233ac71efe963ec0e
elboffin <olamide.adetula.1744@gmail.com>
2022-06-02 17:00:20 -0700

    size

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,18 +1,17 @@
 #include <stdio.h>
 
-/*
- * main - entry point
+/**
+ *	 main - entry point
  *
- * Description : printf
+ *	 Description : printf
  *
- * Return 0 (success)
+ *	 Return 0 (success)
  *
  * */
  
  int main (void)
-{ 
-	printf("with proper grammar, but the outcome is apiece of art,\n");
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return(0);
 
 }
-
