349c732a17bb74b4dccdb2b4799d9dad1ac95a2e
elboffin <olamide.adetula.1744@gmail.com>
2022-06-03 20:07:09 -0700

    print alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+#include <stdlib.h>
+
+#include <time.h>
+
+
+
+/**
+ * main - print last digit in random number
+ *
+ *
+ *
+ * print the letters of the alphabet
+ *
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+int i = 97;
+while (i < 123)
+{
+putchar(i);
+i++;
+}
+putchar(10);
+return (0);
+}
