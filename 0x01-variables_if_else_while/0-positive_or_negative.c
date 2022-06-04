f524bfedea229f41b4d38786691bb1891f7c5f69
elboffin <olamide.adetula.1744@gmail.com>
2022-06-03 19:30:43 -0700

    random number variable

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,32 @@
+#include <stdlib.h>
+
+#include <time.h>
+
+/* more headers goes there */
+
+#include <stdio.h>
+
+
+
+/**
+ *main- entry point
+ *
+ * This is program will assign arandom number to the variable n each time
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+int n;
+
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+if (n == 0)
+	printf("%d is zero\n", n);
+else if (n < 0)
+	printf("% d is negative\n", n);
+else
+	printf("%d is positve\n", n);
+return (0);
+}
