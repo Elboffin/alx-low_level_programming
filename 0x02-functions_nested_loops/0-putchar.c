e6ac051a45adc7dbf9eb3b4fa9e5c1ec3d403f39
elboffin <olamide.adetula.1744@gmail.com>
2022-06-07 10:27:56 -0700

    function

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+/*
+ * main - program entry point
+ *
+ *Return 0 (success)
+ */
+
+int main (void)
+{
+ char l = "task"; 
+ int i = 0;
+ for (; i < 4; i++)
+{ _putchar(l[i]);
+}
+_putchar('\n');
+return (0);
+}
