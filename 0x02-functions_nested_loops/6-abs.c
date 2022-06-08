7ba24c2e700044d4f1338e89c5b016ce4a4d6be2
elboffin <olamide.adetula.1744@gmail.com>
2022-06-07 22:42:57 -0700

    absolute

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _abs - computes the absolute value of an integer
+ *
+ * @a: a is an integer
+ *
+ * Return: integer
+ */
+
+int _abs(int a)
+{
+int n = abs(a);
+return (n);
+}
