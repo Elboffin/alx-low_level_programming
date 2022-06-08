230cdc777bd43ad35dca5ac4efe9d684ad2b65a4
elboffin <olamide.adetula.1744@gmail.com>
2022-06-07 17:18:15 -0700

    alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -10,12 +10,15 @@
 void print_alphabet_x10(void)
 {int round = 0;
 char alp = 'a';
-while (alp <= 'z';)
+while (round <= 10)
+{
+alp ='a';
+while (alp <= 'z');
 {
 _putchar(alp);
 alp++;
 }
 _putchar('\n');
-round++
+round++;
 }
-
+}
